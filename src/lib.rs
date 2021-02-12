b4b850b4f54b4d606cbfb9a96917c395b4823bc2
Malthe Rasmussen <malthesebro@protonmail.com>
2021-02-12 14:03:36 +0100

    feat: Core ChromPos trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub trait ChromPos {
+    fn chrom(&self) -> &str;
+
+    fn pos(&self) -> u32;
+
+    fn colocated(&self, other: &Self) -> bool {
+        self.chrom() == other.chrom() && self.pos() == other.pos()
+    }
+}
+
+impl<T> ChromPos for (T, u32)
+where
+    T: AsRef<str>
+{
+    fn chrom(&self) -> &str {
+        self.0.as_ref()
+    }
+
+    fn pos(&self) -> u32 {
+        self.1
     }
 }
