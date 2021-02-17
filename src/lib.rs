8eae6cf7adbda88ec5072c876d6203feae4222dd
Malthe Rasmussen <malthesebro@protonmail.com>
2021-02-17 19:04:58 +0100

    refactor: Change name of postion equality to 'intersect'

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,27 @@ mod rust_htslib;
 
 pub use self::{chrom_dict::ChromDict, merge::Merge};
 
+/// A genomic position.
+///
+/// Trait for an entity whose location along a genome can be described by an integer coordinate
+/// along some chromosome (or similar, e.g. contig).
 pub trait ChromPos {
+    /// Get the chromosome ID.
     fn chrom(&self) -> &str;
 
+    /// Get the position along the chromosome.
     fn pos(&self) -> u32;
 
-    fn colocated(&self, other: &Self) -> bool {
+    /// Check whether two position are on the same chromosome with the same position along that
+    /// chromosome.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use merge_bio::ChromPos;
+    /// assert!(("1", 1).intersect(&("1", 1)));
+    /// ```
+    fn intersect(&self, other: &Self) -> bool {
         self.chrom() == other.chrom() && self.pos() == other.pos()
     }
 }
